d009914ff6d9b58fa24e878178b619ed20f2b5a6
Ted1687 <damoieric16@gmail.com>
2025-10-06 09:53:21 -0400

    Initial Commit & Script Camera Rotate implémenter

---
+++ tp2-ec-lc/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Camera mainCam;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        mainCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+       
+    }
+}
---
+++ tp2-ec-lc/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+
+    public float rotationSpeed = 50f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");  // -1 pour gauche, 1 pour droite
+
+        //transform.Rotate(0, horizontalInput * rotationSpeed * Time.deltaTime, 0);//Mouv autour de y
+
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);//Mouv autour de y
+    }
+}
