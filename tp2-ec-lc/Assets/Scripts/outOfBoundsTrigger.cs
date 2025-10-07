9068f9bce5fa3f58684ee428b787d01ca202f362
Lassana Cisse <2341056@cegeplimoilou.ca>
2025-10-07 00:55:11 +0000

    Ajout d'un cube et d'un trigger pour detecter la sortie du joueur dans le ring

---
+++ tp2-ec-lc/Assets/Scripts/outOfBoundsTrigger.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class OutOfBoundsTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.CompareTag("Enemy")) // Est ce un enemy ?
+        {
+            Destroy(other.gameObject);  // Détruit l'ennemi
+            LevelController.instance.EnemyOutOfBound();
+        }
+
+        
+        else if (other.CompareTag("Player")) // Est ce un joueur ?
+        {
+            LevelController.instance.GameOver();  // fin de jeu
+        }
+    }
+}
